02a710221d837e134ce8a9e967ab86e489bf5607
Glenn Salaman <g.salaman@comcast.net>
2021-03-17 16:40:00 -0600

    simple print function

--- get_buttons.py
+++ get_buttons.py
@@ -1,21 +1,29 @@
 import serial
 import time
 
+def get_button():
+  global ser
+
+  if ser.inWaiting() == 0:
+    return None
+  
+  line = ser.readline()
+  line = line.rstrip()
+
+  print("Line: "+ line)
+
+
+  ser.flushInput() 
 
 print("running")
+ser = serial.Serial("/dev/tty.usbserial-DN02Z5R6", 9600, timeout=1) 
+time.sleep(0.01) # wait for serial port to open.
+if ser.isOpen():
+  print("connected!")
 
-with serial.Serial("/dev/tty.usbserial-DN02Z5R6", 9600, timeout=1) as ser:
-  time.sleep(0.01) # wait for serial port to open.
-  if ser.isOpen():
-    print("connected!")
+  while True:
+    get_button()
+    time.sleep(0.01)
 
-    while True:
-
-      while ser.inWaiting()==0:
-        pass
-      if ser.inWaiting() > 0:
-        line = ser.readline()
-        print(line)
-        ser.flushInput() 
 
 
